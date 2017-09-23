55f7894704e68b4def0753a5d9535cbf6cf9436e
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-09-23 11:53:09 +0200

    Ejercicio A

--- src/Enunciado/A.java
+++ src/Enunciado/A.java
@@ -1,5 +1,27 @@
 package Enunciado;
 
-public class A {
+import java.util.Scanner;
 
+public class A {
+	
+	public static void main(String args []){
+		Scanner entrada = new Scanner(System.in);
+		int[] num = new int[3];
+		
+		//Pido numeros
+		for (int i=0; i<3; i++){
+			System.out.println("Introduce el " + (i+1) + "º número");
+			num[i] = entrada.nextInt();
+		}
+		
+		//Si es mayor que 0
+		if (num[0] > 0){
+			//Calculo multiplicación
+			System.out.println("El resultado de " + num[1] + " x " + num[2] + " es " + (num[1] * num[2]));
+		} else {
+			//Calculo suma
+			System.out.println("El resultado de " + num[1] + " + " + num[2] + " es " + (num[1] + num[2]));
+		}
+		
+	}
 }
